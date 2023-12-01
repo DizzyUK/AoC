52c8f719357a12086257b78cfca67bb7e9c6e218
Meg Challinor <116823812+DizzyUK@users.noreply.github.com>
2023-12-01 23:46:14 +0000

    Input and Part1 Done

--- 2023/01/part1.ps1
+++ 2023/01/part1.ps1
@@ -1,1 +1,13 @@
-write-host "Part 1 Has Completed"
+$input = get-content ".\input.txt"
+
+$totalcount = 0
+
+foreach ($line in $input) {
+    $digits = ($line | select-string -pattern '\d' -AllMatches)
+    $firstdigit = $digits.matches[0].Value
+    $lastdigit = $digits.matches[-1].Value
+    [int]$fullnumber = $firstdigit + $lastdigit
+    $totalcount += $fullnumber
+}
+
+write-host "$totalcount"
