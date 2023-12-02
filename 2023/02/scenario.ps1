f17310efbceb6e28badf2f60c2c7a469d3f05060
Meg Challinor <116823812+DizzyUK@users.noreply.github.com>
2023-12-02 14:50:07 +0000

    Day2 Pt2 added

--- 2023/02/scenario.ps1
+++ 2023/02/scenario.ps1
@@ -53,3 +53,17 @@ $legalcount
 
 #### Part2 ####
 
+$totalcubepower = 0
+foreach ($gameplayed in $matchinput.GetEnumerator()) {
+    $highestblue = 0
+    $highestred = 0
+    $highestgreen = 0
+    foreach ($setplayed in $gameplayed.Value.values) {
+        if ($setplayed.blue -gt $highestblue) {$highestblue = $setplayed.blue}
+        if ($setplayed.red -gt $highestred) {$highestred = $setplayed.red}
+        if ($setplayed.green -gt $highestgreen) {$highestgreen = $setplayed.green}
+    }
+    $gamecubepwr = $highestblue * $highestred * $highestgreen
+    $totalcubepower += $gamecubepwr
+}
+$totalcubepower
