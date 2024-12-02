e8082a613703bb88ce63f79244232e02e91e68e9
DizzyUK <megchallinor83+github@gmail.com>
2024-12-02 16:29:03 +0000

    changed to using a function for the testing, added part 2, unsure why it doesnt work yet

--- 2024/02/scenario.ps1
+++ 2024/02/scenario.ps1
@@ -1,15 +1,15 @@
 $inputTxt = get-content ".\input.txt"
-
-#Part1
-$goodCounter = 0
-foreach ($line in $inputTxt) {
-    [int32[]]$readings = ($line.split(" ",[System.StringSplitOptions]::RemoveEmptyEntries))
-    $index = 0
+function test-linesafety {
+    Param (
+        [int32[]]$readings
+    )
     $readingLevels = [PSCustomObject]@{
         line = $line
         readingSafe = $true
         readingChange = ""
     }
+
+    $index = 0
     do {
         $readingDiff = ($readings[$index] - $readings[$index + 1])
         if ($readingDiff -lt 0) {
@@ -26,15 +26,39 @@ foreach ($line in $inputTxt) {
         }
         Elseif ($readingDiff -eq 0) {$readingLevels.readingSafe =$false}
         if ([Math]::Abs($readingDiff) -gt 3) {$readingLevels.readingSafe = $false}
-        if ($readingLevels.readingSafe -eq $false) {Break}
+        if ($readingLevels.readingSafe -eq $false) {return $false}
         $index++
     } until ($index -ge ($readings.length -1))
-    if ($readingLevels.readingSafe -eq $true) {$goodCounter++}
+    return $true
+}
+
+#Part1
+
+$goodCounter = 0
+foreach ($line in $inputTxt) {
+    [int32[]]$readings = ($line.split(" ",[System.StringSplitOptions]::RemoveEmptyEntries))
+    if (test-linesafety $readings) {$goodCounter++}
 }
 
 write-host "Part 1: $goodCounter"
 
 #Part2
 
+$advGoodCounter = 0
+foreach ($line in $inputTxt) {
+    [System.Collections.Generic.List[int]]$readings = ($line.split(" ",[System.StringSplitOptions]::RemoveEmptyEntries))
+    if (test-linesafety $readings) {
+        $advGoodCounter++
+        continue
+    }
+    else {
+        for ($i = 0; $i -lt $readings.count; $i++) {
+            if (test-linesafety ($readings | where-object {$readings.IndexOf($_) -ne $i})) {
+                $advGoodCounter++
+                break
+            }
+        }
+    }
+}
 
-#write-host "Part 2: $listSimilarity"
+write-host "Part 2: $advgoodCounter"
