cdadf203483c3b94082c7c1212d69032a0e6ba13
DizzyUK <megchallinor83+github@gmail.com>
2024-12-02 11:12:43 +0000

    Some minor efficiency fixes

--- 2024/01/scenario.ps1
+++ 2024/01/scenario.ps1
@@ -5,7 +5,7 @@ $inputTxt = get-content ".\input.txt"
 $leftList = @()
 $rightList = @()
 foreach ($line in $inputTxt) {
-    $leftValue,$rightValue = ($line | select-string -pattern '(\d+)' -AllMatches).matches.value
+    $leftValue,$rightValue = ($line.split(" ",[System.StringSplitOptions]::RemoveEmptyEntries))
     $leftList += [int]$leftValue
     $rightList += [int]$rightValue
 }
@@ -16,8 +16,7 @@ $rightList = $rightList | sort-object
 $runningTotal = 0
 $index = 0
 do {
-    if ($leftList[$index] -gt $rightList[$index]) {$runningTotal += ($leftList[$index] - $rightList[$index])}
-    if ($leftList[$index] -lt $rightList[$index]) {$runningTotal += ($rightList[$index] - $leftList[$index])}
+    $runningTotal += [Math]::Abs($leftList[$index] - $rightList[$index])
     $index++
 } until ($index -eq $leftList.length)
 
