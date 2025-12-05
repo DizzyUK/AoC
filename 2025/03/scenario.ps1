9ef7f4cdb2bd9ba0ad5f05399f0d1c099c1a3c63
DizzyUK <116823812+DizzyUK@users.noreply.github.com>
2025-12-05 19:56:06 +0000

    Day 3 pt 2 complete

--- 2025/03/scenario.ps1
+++ 2025/03/scenario.ps1
@@ -21,5 +21,30 @@ write-host "Part 1: $joltageCount"
 
 #Part2
 
+function find-number {
+    param(
+        $bank,
+        $startPos,
+        $numberToFind
+    )
+    $numbersLeftToFind = 12 - $numberToFind
+    $availableBank = [char[]]$bank[($startPos+1)..($bank.length - ($numbersLeftToFind+1))] 
+    $bankChars = $availableBank | Sort-Object -Descending
+    $currentPosition = $availableBank.IndexOf($bankChars[0]) + ($startPos+1)
+    return $bankChars[0],$currentPosition
+}
 
-#write-host "Part 2: $invalidIDsPt2"
+[int64]$joltageCountPt2 = 0
+foreach ($bank in $inputTxt) {
+    $i = 0
+    $currentNoPos = -1
+    [string]$bankJolts = ""
+    do {
+        $currentNo,$currentNoPos = find-number -bank $bank -startPos $currentNoPos -numberToFind ($i+1)
+        $bankJolts = $bankJolts + $currentNo
+        $i++
+    } until ($i -ge 12)
+    $joltageCountPt2 += [int64]$bankJolts
+}
+
+write-host "Part 2: $joltageCountPt2"
