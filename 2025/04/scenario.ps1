3529d20526ffdc112b4e04d8f8d26c28d9ca0820
DizzyUK <116823812+DizzyUK@users.noreply.github.com>
2025-12-09 15:27:36 +0000

    way too slow with the approach I made, changed it to make less calls to the list array, it was going to take about 27 hours to run!

--- 2025/04/scenario.ps1
+++ 2025/04/scenario.ps1
@@ -5,21 +5,34 @@ $inputTxt = get-content ".\input.txt"
 
 function check-neighbours {
     param (
-        [System.Collections.ArrayList]$gridLayout,
+        [System.Collections.ArrayList]$grid,
         [int32]$inputX,
         [int32]$inputY
     )
-    $flatGrid = $gridLayout | ForEach-Object {$_}
-    $topLeft = ($flatGrid| where-object {($_.x -eq ($inputX -1)) -and ($_.y -eq ($inputY - 1))}).contents
-    $topMid = ($flatGrid| where-object {($_.x -eq ($inputX)) -and ($_.y -eq ($inputY - 1))}).contents
-    $topRight = ($flatGrid| where-object {($_.x -eq ($inputX +1)) -and ($_.y -eq ($inputY - 1))}).contents
-    $midLeft = ($flatGrid| where-object {($_.x -eq ($inputX -1)) -and ($_.y -eq ($inputY))}).contents
-    $midRight = ($flatGrid| where-object {($_.x -eq ($inputX +1)) -and ($_.y -eq ($inputY))}).contents
-    $bottomLeft = ($flatGrid| where-object {($_.x -eq ($inputX -1)) -and ($_.y -eq ($inputY + 1))}).contents
-    $bottomMid = ($flatGrid| where-object {($_.x -eq ($inputX)) -and ($_.y -eq ($inputY + 1))}).contents
-    $bottomRight = ($flatGrid| where-object {($_.x -eq ($inputX + 1)) -and ($_.y -eq ($inputY + 1))}).contents
-    $countVars = @($topLeft, $topMid, $topRight, $midLeft, $midRight, $bottomLeft, $bottomMid, $bottomRight)
-    $count = ($countVars | where-object {$_ -eq "@"}).Count
+
+    foreach ($xShift in -1..1) {
+        foreach ($yShift in -1..1) {
+            if (($xShift -eq 0) -and ($yShift -eq 0)) {continue}
+            $cellX = $inputX + $xShift
+            $cellY = $inputY + $yShift
+            if ((($cellX -ge 0) -and ($cellY -lt $grid.count)) -and (($cellY -ge 0) -and ($cellN -lt $grid[$cellY].count))) {
+                if ($grid[$cellY][$cellX].contents -eq "@") {
+                    $count++
+                }
+            }
+        }
+    }
+
+    # $topLeft = ($flatGrid| where-object {($_.x -eq ($inputX -1)) -and ($_.y -eq ($inputY - 1))}).contents
+    # $topMid = ($flatGrid| where-object {($_.x -eq ($inputX)) -and ($_.y -eq ($inputY - 1))}).contents
+    # $topRight = ($flatGrid| where-object {($_.x -eq ($inputX +1)) -and ($_.y -eq ($inputY - 1))}).contents
+    # $midLeft = ($flatGrid| where-object {($_.x -eq ($inputX -1)) -and ($_.y -eq ($inputY))}).contents
+    # $midRight = ($flatGrid| where-object {($_.x -eq ($inputX +1)) -and ($_.y -eq ($inputY))}).contents
+    # $bottomLeft = ($flatGrid| where-object {($_.x -eq ($inputX -1)) -and ($_.y -eq ($inputY + 1))}).contents
+    # $bottomMid = ($flatGrid| where-object {($_.x -eq ($inputX)) -and ($_.y -eq ($inputY + 1))}).contents
+    # $bottomRight = ($flatGrid| where-object {($_.x -eq ($inputX + 1)) -and ($_.y -eq ($inputY + 1))}).contents
+    # $countVars = @($topLeft, $topMid, $topRight, $midLeft, $midRight, $bottomLeft, $bottomMid, $bottomRight)
+    # $count = ($countVars | where-object {$_ -eq "@"}).Count
     return $count
 }
 
@@ -46,12 +59,10 @@ $rollsCount = 0
 $flatGrid = $gridLayout | ForEach-Object {$_}
 foreach ($gridLoc in $flatgrid) {
     if ($gridLoc.contents -eq "@") {
-        if ((check-neighbours -grid $flatGrid -inputX $($gridLoc.x) -inputY $($gridLoc.y)) -le 3) {$rollsCount++}
+        if ((check-neighbours -grid $gridLayout -inputX $($gridLoc.x) -inputY $($gridLoc.y)) -le 3) {$rollsCount++}
     }
 }
 
 write-host "Part 1: $rollsCount"
 
-#Part2
 
-#write-host "Part 2: "
